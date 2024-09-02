6391e0156130fd381a71d88d40946a5fd413a6cc
bibovaldez <donnashane.f.ventura@isu.edu.ph>
2024-09-02 19:57:19 +0800

    logout email

--- resources/views/emails/logout.blade.php
+++ resources/views/emails/logout.blade.php
@@ -1,9 +1,14 @@
 <p>Dear {{ Auth::user()->name }},</p>
 
-<p>You have logged into multiple sessions. If you want to log out from all other sessions, click the link below:</p>
+<p>{{ __('You have logged into multiple sessions. If you want to log out from all other sessions, click the link below:') }}
+</p>
 
 <p><a href="{{ $logoutLink }}">Logout from other sessions</a></p>
 
-<p>If you did not perform this action, please ignore this email.</p>
+<p>{{ __('If you did not expect to receive an invitation to this team, you may discard this email.') }}</p>
 
-<p>Regards,<br>Bioset</p>
+{{ __('Regards,') }}<br>
+{{ config('app.name') }}
+
+{{ __('If you’re having trouble clicking the "Logout from other sessions" button, copy and paste the URL below into your web browser:') }}
+[{{ $logoutLink }}]({{ $logoutLink }})
