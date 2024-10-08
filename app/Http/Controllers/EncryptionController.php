99504d67f2e248bdf0d73278b6a1bcbf7ddcb3b7
bibovaldez <donnashane.f.ventura@isu.edu.ph>
2024-10-08 13:46:01 +0800

    Revert "refactor: Update EncryptionController.php and web.php"
    This reverts commit d8197bca79604ea15bb8e80d6ab21be51748c117.

--- app/Http/Controllers/EncryptionController.php
+++ app/Http/Controllers/EncryptionController.php
@@ -8,7 +8,7 @@ class EncryptionController extends Controller
     protected $key;
     protected $additionalData;
 
-    private function __construct()
+    public function __construct()
     {
         $this->key = base64_decode(config('custom.encryption.key'));
         $this->additionalData = hash('sha256', config('custom.encryption.additional_data'), true);
